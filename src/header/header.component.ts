e14fd2b9a60abdb3cca1e358152e6c9dee7759bf
Jamie Nottage <jamie.nottage@outlook.com>
2021-11-25 10:52:09 +0000

    Adding binding

--- src/header/header.component.ts
+++ src/header/header.component.ts
@@ -5,7 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  // data or Model or properties
+  title: string = 'HEADER COMPONENT';
+  fname: string = 'john';
+  person = {
+    id: 100,
+    name: 'jill',
+    age: 18,
+  };
+  photo = 'assets/images/user.png';
+
   constructor() {
     console.log('HeaderComponent Loaded...');
+    console.log(`Title property - ${this.title}`);
   }
 }
